4c2372d20b8c4911f7e94099e5bc5c7c72c4f006
James Salvatore <jimmy.c.salvatore@gmail.com>
2026-05-28 01:08:38 -0500

    chore: adds all weapons for hollow_knight;

---
+++ bizhawk_scripts/snes/ffmq/all_weapons.lua
@@ -0,0 +1,48 @@
+---@type table
+local memory_maps = {{
+    --[[
+        Weapons are at 0x001032 and 0x001033.
+        The system is a bit weird, but increasing the value of this byte 
+        will give different combinations of weapons.  A value of FF will 
+        give all weapons.
+    ]]
+    name = "All Weapons.",
+    address = '0x001032',
+    patch_value = '0xFF',
+    notes = [[You will have to reload the item list or use an item before you can see the effect.]]
+}, {
+    name = "All Weapons (2).",
+    address = '0x001033',
+    patch_value = '0xFF',
+    notes = [[You will have to reload the item list or use an item before you can see the effect.]]
+}}
+
+--[[
+    Loop over all of the values in the table above,
+    log the previous value for the user and set the new
+    patch values.
+]]
+
+for _, mapping in ipairs(memory_maps) do
+    local name = mapping.name
+    local address_str = mapping.address
+    local patch_value_str = mapping.patch_value
+    local notes = mapping.notes
+
+    local address = tonumber(address_str)
+    local patch_value = tonumber(patch_value_str)
+
+    local previous_value = memory.readbyte(address)
+    local previous_value_str = string.format("%X", previous_value)
+
+    console.log('\n--------------------')
+    console.log('Patch: ' .. name)
+    console.log('Address: ' .. address_str)
+    console.log('Previous value: ' .. previous_value_str)
+
+    -- Patch the value.
+    memory.writebyte(address, patch_value)
+
+    console.log('Current value: ' .. patch_value_str .. '.')
+    console.log('NOTE: ' .. notes)
+end
